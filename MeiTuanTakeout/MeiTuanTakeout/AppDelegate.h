d3387f9f111f8a73631b53526a1b0f88d31c0030
SexyCodingBear <joe_fight@126.com>
2017-07-31 00:20:41 +0800

    项目准备

---
+++ MeiTuanTakeout/MeiTuanTakeout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiTuanTakeout
+//
+//  Created by 温岱阳 on 2017/7/31.
+//  Copyright © 2017年 DaiyangWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiTuanTakeout/MeiTuanTakeout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiTuanTakeout
+//
+//  Created by 温岱阳 on 2017/7/31.
+//  Copyright © 2017年 DaiyangWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
