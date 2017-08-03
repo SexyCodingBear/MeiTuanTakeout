bc2141351798761f056d502845e36c40c71287de
SexyCodingBear <joe_fight@126.com>
2017-08-03 12:12:25 +0800

    文件夹位置移动

---
+++ MeiTuanTakeout/MeiTuanTakeout/Classes/ShopDetail/Controller/MeiTuanShopDetailController.h
@@ -0,0 +1,19 @@
+//
+//  MeiTuanShopDetailController.h
+//  MeiTuanTakeout
+//
+//  Created by 温岱阳 on 2017/8/2.
+//  Copyright © 2017年 DaiyangWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "WDYInfoModel.h"
+#import "WDYInfoView.h"
+#import "MeiTuanShopHeaderViewModel.h"
+
+@interface MeiTuanShopDetailController : UIViewController
+
+// 声明头部视图模型
+@property (strong,nonatomic) MeiTuanShopHeaderViewModel *headerViewModel;
+
+@end
--- MeiTuanTakeout/MeiTuanTakeout/Classes/ShopDetail/Other/AnimatorTransitionDelegate.h
+++ MeiTuanTakeout/MeiTuanTakeout/Classes/ShopDetail/Other/AnimatorTransitionDelegate.h
@@ -8,6 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-@interface AnimatorTransitionDelegate : NSObject
+@interface AnimatorTransitionDelegate : NSObject <UIViewControllerTransitioningDelegate>
 
 @end
